ab2a15804ed7287deb5551f1871e2bce651293ef
budy6991 <alvmenfer@gmail.com>
2022-10-29 00:32:39 +0300

    Tried browser router on app.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Page } from "./components/Page";
 
 function App() {
   return (
     <div className="w-screen h-screen ">
-      <HashRouter basename="/">
-        <Route path="/harry-potter-routing-demo" element={<Home />} />
-        <Route
-          path="houses"
-          element={
-            <Page
-              url={"https://wizard-world-api.herokuapp.com/Houses"}
-              title="Houses"
-            />
-          }
-        />
-        <Route
-          path="wizards"
-          element={
-            <Page
-              url="https://wizard-world-api.herokuapp.com/Wizards"
-              title="Wizards"
-            ></Page>
-          }
-        />
-        <Route
-          path="spells"
-          element={
-            <Page
-              url="https://wizard-world-api.herokuapp.com/Spells"
-              title="Spells"
-            />
-          }
-        />
-      </HashRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/harry-potter-routing-demo" element={<Home />} />
+          <Route
+            path="houses"
+            element={
+              <Page
+                url={"https://wizard-world-api.herokuapp.com/Houses"}
+                title="Houses"
+              />
+            }
+          />
+          <Route
+            path="wizards"
+            element={
+              <Page
+                url="https://wizard-world-api.herokuapp.com/Wizards"
+                title="Wizards"
+              ></Page>
+            }
+          />
+          <Route
+            path="spells"
+            element={
+              <Page
+                url="https://wizard-world-api.herokuapp.com/Spells"
+                title="Spells"
+              />
+            }
+          />
+          <Route path="*" component={404} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="harry-potter-routing-demo">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
